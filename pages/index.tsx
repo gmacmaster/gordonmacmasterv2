e251a34757290e8dd101dfbaadb4f60bb76bad84
Gordon MacMaster <gordon@herdpay.com>
2023-01-05 13:27:26 -0700

    customization'

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            Gordon MacMaster
           </p>
           <div>
             <a
